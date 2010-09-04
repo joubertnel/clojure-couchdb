3f2e3cf4b173e83063adc7bb038eff1d729cbeaf
Joubert Nel <joubert@joubster.com>
2010-09-04 06:07:33 -0400

    Reference newly pushed clojure-http-client fork on Clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clojure-couchdb "0.2.3"
+(defproject clojure-couchdb "0.2.4"
   :description "Clojure interface for Apache CouchDB"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [clojure-http-client "1.0.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+                 [org.clojars.joubert/clojure-http-client "1.1.0"]])
