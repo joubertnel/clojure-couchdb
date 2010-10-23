e21e53d0375da3ba44d2444f5139fb0c918dfa16
Joubert Nel <joubert@joubster.com>
2010-10-23 09:50:41 +0200

    Bumped version to 0.4.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-couchdb "0.4.4"
+(defproject clojure-couchdb "0.4.5"
   :description "Clojure interface for Apache CouchDB"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
